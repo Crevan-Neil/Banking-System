73f3e72ecfd3af35cc18180aef7272294ddf1430
Crevan Neil Fernandes <crevanneilf@gmail.com>
2026-03-03 12:16:27 +0530

    Fixed Balance Issues

--- server/src/routes/account.routes.js
+++ server/src/routes/account.routes.js
@@ -1,11 +1,11 @@
-const express=require("express");
-const {authMiddleware}=require("../middlewares/auth.middleware");
-const { createAccountController, getUserController, getAccountBalanceController }=require("../controllers/account.controller");
-const router=express.Router();
+const express = require("express");
+const { authMiddleware } = require("../middlewares/auth.middleware");
+const { createAccountController, getUserController, getAccountBalanceController } = require("../controllers/account.controller");
+const router = express.Router();
 
 
-router.post("/",authMiddleware,createAccountController);
-router.get("/",authMiddleware,getUserController);
-router.get("/balance/:accountid",authMiddleware, getAccountBalanceController);
+router.post("/", authMiddleware, createAccountController);
+router.get("/", authMiddleware, getUserController);
+router.get("/balance/:accountId", authMiddleware, getAccountBalanceController);
 
-module.exports=router;
+module.exports = router;
